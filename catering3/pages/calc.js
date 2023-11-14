4f38852732c9c7ff850c3bb21b44a096581fa6d5
itkosobokov <itkosobokov@gmail.com>
2023-11-14 23:50:02 +0300

    добавлены слушатели на кнопки и вывод в консоль

--- catering3/pages/calc.js
+++ catering3/pages/calc.js
@@ -1,5 +1,22 @@
-var buttunPlus = document.getElementById("buttunPlus");
+var buttunPlus = document.getElementById("buttonPlus");
 var buttonMinus = document.getElementById("buttonMinus");
 var buttonMultyply = document.getElementById("buttonMultyply");
 var buttonDevide = document.getElementById("buttonDevide");
 
+function onButtonPlusClick(){
+    console.log("onButtonPlusClick");
+}
+function onButtonMinusClick(){
+    console.log("onButtonMinusClick");
+}
+function onButtonMultyplyClick(){
+    console.log("onButtonMultyplyClick");
+}
+function onButtonDevideClick(){
+    console.log("onButtonDevideClick");
+}
+
+buttunPlus.addEventListener('click', onButtonPlusClick);
+buttonMinus.addEventListener('click', onButtonMinusClick);
+buttonMultyply.addEventListener('click', onButtonMultyplyClick);
+buttonDevide.addEventListener('click', onButtonDevideClick);
