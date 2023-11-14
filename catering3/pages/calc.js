1095f0b23b53bdd505bd7412f504d2ecfb2f4584
itkosobokov <itkosobokov@gmail.com>
2023-11-14 23:13:28 +0300

    объявлены переменные в жс для кнопок

---
+++ catering3/pages/calc.js
@@ -0,0 +1,5 @@
+var buttunPlus = document.getElementById("buttunPlus");
+var buttonMinus = document.getElementById("buttonMinus");
+var buttonMultyply = document.getElementById("buttonMultyply");
+var buttonDevide = document.getElementById("buttonDevide");
+
