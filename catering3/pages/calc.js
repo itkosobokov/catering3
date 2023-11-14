6e99509d5df0e9e055dfb1b742b61fa2c30fdbcb
itkosobokov <itkosobokov@gmail.com>
2023-11-15 00:13:48 +0300

    доделан каолькулятор из 15 урока с выводом  алерта

--- catering3/pages/calc.js
+++ catering3/pages/calc.js
@@ -5,15 +5,51 @@ var buttonDevide = document.getElementById("buttonDevide");
 
 function onButtonPlusClick(){
     console.log("onButtonPlusClick");
+    var input1 = document.getElementById("number1");
+    var input2 = document.getElementById("number2");
+
+    var number1 = Number(input1.value);
+    var number2 = Number(input2.value);
+
+    var result = number1 + number2;
+
+    window.alert(result);
 }
 function onButtonMinusClick(){
     console.log("onButtonMinusClick");
+    var input1 = document.getElementById("number1");
+    var input2 = document.getElementById("number2");
+
+    var number1 = Number(input1.value);
+    var number2 = Number(input2.value);
+
+    var result = number1 - number2;
+
+    window.alert(result);
 }
 function onButtonMultyplyClick(){
     console.log("onButtonMultyplyClick");
+    var input1 = document.getElementById("number1");
+    var input2 = document.getElementById("number2");
+
+    var number1 = Number(input1.value);
+    var number2 = Number(input2.value);
+
+    var result = number1 * number2;
+
+    window.alert(result);
 }
 function onButtonDevideClick(){
     console.log("onButtonDevideClick");
+    var input1 = document.getElementById("number1");
+    var input2 = document.getElementById("number2");
+
+    var number1 = Number(input1.value);
+    var number2 = Number(input2.value);
+
+    var result = number1 / number2;
+
+    window.alert(result);
 }
 
 buttonPlus.addEventListener('click', onButtonPlusClick);
