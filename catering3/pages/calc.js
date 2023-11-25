721dc7d925a9a46d7a13818e3d9b9d9a5a7b01ac
itkosobokov <itkosobokov@gmail.com>
2023-11-25 23:35:21 +0300

    бэкап до 17 урока провал с массивом и с фор

--- catering3/pages/calc.js
+++ catering3/pages/calc.js
@@ -35,19 +35,7 @@ function onOperationButtonClick(eventObject) {
     makeOperation(operation);
 }
 
-function addCommonEventListener(i) {
-    operationButtons[i].addEventListener('click', onOperationButtonClick);
-}
-
-var operationButtons = [buttonPlus, buttonMinus, buttonMultyply, buttonDevide]
-
-addCommonEventListener(0);
-addCommonEventListener(1);
-addCommonEventListener(2);
-addCommonEventListener(3);
-
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
-}
-
+buttonPlus.addEventListener('click', onOperationButtonClick);
+buttonMinus.addEventListener('click', onOperationButtonClick);
+buttonMultyply.addEventListener('click', onOperationButtonClick);
+buttonDevide.addEventListener('click', onOperationButtonClick);
